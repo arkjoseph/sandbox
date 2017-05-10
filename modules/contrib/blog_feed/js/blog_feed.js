43e2bd4c941603597c0cea0c77ef352e4b49a37c
joseph garrido <joseph.garrido@associates.hq.dhs.gov>
2017-05-10 14:50:05 -0700

    added yahoo YQL api url to avoid CORS

--- modules/contrib/blog_feed/js/blog_feed.js
+++ modules/contrib/blog_feed/js/blog_feed.js
@@ -1,45 +1,46 @@
 /**
+ *
  * @file
- * Contains js the jobs api json feed.
+ * Contains js the blog_feed xml/rss feed.
+ * Yahoo YQL is preparing the data for XHR distribution
+ * https://developer.yahoo.com/yql/
+ *
  */
 
-
-
 (function ($, Drupal) {
-  Drupal.behaviors.usaJobs = {
+  'use strict';
+
+  Drupal.blog_feed = {};
+  Drupal.behaviors.blog_feed = {
     attach: function (context, settings) {
-      'use strict';
-      var $authKey = "bLTvp99I05OBhwJAkVSpRv8QfCp9eN+FRqPi7DoIbT0=";
-
-      $.ajax({
-        url: "https://data.usajobs.gov/api/search?K=GSA&l=United+States&ResultsPerPage=5",
-        type: "GET",
-        headers: {
-          "Authorization-Key": $authKey,
-          "Content-Type": "application/json"
-        },
-        //async: true,
-        //cache: true,
-        dataType: "json",
-        success: function(data){
-          $.each(data.SearchResult.SearchResultItems, function(key,value){
-            var value      = this.MatchedObjectDescriptor,
-              title      = value.PositionTitle,
-              positionid = value.PositionID,
-              uri        = value.PositionURI,
-              local      = value.PositionLocationDisplay,
-              desc       = value.UserArea.Details.JobSummary;
-
-            $(".json-content").append("<li>" +
-              "<a target='_blank' href='"+ uri +"' data-position-id='" + positionid + "'>" + title + "</a> &mdash; '"+ local +"' " +
-              "<ul><li><p>"+ desc +"</p></li>" +
-              "<li><a target='_blank' href='"+ uri +"?PostingChannelID=RESTAPI'><b>Apply</b></a></li></ul>" +
-              "</li>"
-            );
-          });
-        }
-      });
+      Drupal.blog_feed.feed();
     }
+  };
+
+  //
+  // Main jobs feed ajax get request
+  //
+
+  Drupal.blog_feed.feed = function () {
+    var feed = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20xml%20WHERE%20url%3D%22https%3A%2F%2Fgsablogs.gsa.gov%2Finnovation%2Ffeed%3Fformat%3Dxml%22&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+    $.ajax(feed, {
+      accepts:{
+        xml:"application/rss+xml"
+      },
+      dataType:"xml",
+      success:function(data) {
+        $(data).find("item").each(function () { // or "item" or whatever suits your feed
+          var el = $(this);
+          console.log("------------------------");
+          console.log("title      : " + el.find("title").text());
+          console.log("link       : " + el.find("link").text());
+          console.log("description: " + el.find("description").text());
+        });
+
+
+      }
+    });
   };
 
 
--- modules/contrib/blog_feed/src/Controller/BlogFeedController.php
+++ modules/contrib/blog_feed/src/Controller/BlogFeedController.php
@@ -1,32 +1,32 @@
 <?php
 
-namespace Drupal\jobs_api\Controller;
+namespace Drupal\blog_feed\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 /**
- * Controller for jobs_api.
+ * Controller for blog_feed.
  *
- * @ingroup jobs_api
+ * @ingroup blog_feed
  */
-class JobsApiController extends ControllerBase {
+class BlogFeedController extends ControllerBase {
 
   /**
    * @return array
    *   A renderable array.
    */
-  public function getJobsApiImplementation() {
-    $title = t('Select filters');
+  public function getBlogFeedImplementation() {
+    $title = t('Blog API');
     // Build using our theme. This gives us content, which is not a good
     // practice, but which allows us to demonstrate adding JavaScript here.
     $build['myelement'] = array(
-      '#theme' => 'jobs_api',
+      '#theme' => 'blog_feed',
       '#title' => $title,
     );
     // Add our script. It is tiny, but this demonstrates how to add it. We pass
     // our module name followed by the internal library name declared in
     // libraries yml file.
-    $build['myelement']['#attached']['library'][] = 'jobs_api/jobs_api.api';
+    $build['myelement']['#attached']['library'][] = 'blog_feed/blog_feed.api';
     // Return the renderable array.
     return $build;
   }
